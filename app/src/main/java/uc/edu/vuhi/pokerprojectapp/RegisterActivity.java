98dc43abb28e6b63439c0d807158c6fecc6dd9a0
vuhi <vuhi@mail.uc.edu>
2018-07-07 17:08:31 -0400

    Apply butter knife in RegisterActivity

--- app/src/main/java/uc/edu/vuhi/pokerprojectapp/RegisterActivity.java
+++ app/src/main/java/uc/edu/vuhi/pokerprojectapp/RegisterActivity.java
@@ -17,19 +17,29 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText txtRegisterEmail;
+    @BindView(R.id.txtRegisterEmail)
+    EditText txtRegisterEmail;
 
-    private EditText txtRegisterPassword;
+    @BindView(R.id.txtRegisterPassword)
+    EditText txtRegisterPassword;
 
-    private EditText txtConfirmPassword;
+    @BindView(R.id.txtConfirmPassword)
+    EditText txtConfirmPassword;
 
-    private Button btnCreateNewAccount;
+    @BindView(R.id.btnCreateNewAccount)
+    Button btnCreateNewAccount;
 
-    private Button btnAlreadyHaveAccount;
+    @BindView(R.id.btnAlreadyHaveAccount)
+    Button btnAlreadyHaveAccount;
 
-    private ProgressBar probRegister;
+    @BindView(R.id.probRegister)
+    ProgressBar probRegister;
 
     private FirebaseAuth mAuth;
 
@@ -37,59 +47,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        ButterKnife.bind(this);
         mAuth = FirebaseAuth.getInstance();
-
-        txtRegisterEmail = (EditText)findViewById(R.id.txtRegisterEmail);
-        txtRegisterPassword = (EditText)findViewById(R.id.txtRegisterPassword);
-        txtConfirmPassword = (EditText)findViewById(R.id.txtConfirmPassword);
-        btnCreateNewAccount = (Button)findViewById(R.id.btnCreateNewAccount);
-        btnAlreadyHaveAccount = (Button)findViewById(R.id.btnAlreadyHaveAccount);
-        probRegister = (ProgressBar)findViewById(R.id.probRegister);
-
-        btnCreateNewAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txtRegisterEmailString = txtRegisterEmail.getText().toString();
-                String txtRegisterPasswordString = txtRegisterPassword.getText().toString();
-                String txtConfirmPasswordString = txtConfirmPassword.getText().toString();
-
-                if(!TextUtils.isEmpty(txtRegisterEmailString) && !TextUtils.isEmpty(txtRegisterPasswordString)
-                        && !TextUtils.isEmpty(txtConfirmPasswordString))
-                {
-                    if(txtRegisterPasswordString.equals(txtConfirmPasswordString))
-                    {
-                        probRegister.setVisibility(View.VISIBLE);
-                        mAuth.createUserWithEmailAndPassword(txtRegisterEmailString, txtRegisterPasswordString)
-                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful())
-                                {
-                                    sendToMainActivity();
-                                }else{
-                                    String exceptionMsg = task.getException().getMessage();
-                                    Toast.makeText(RegisterActivity.this, "Error: " + exceptionMsg, Toast.LENGTH_LONG).show();
-                                }
-                                probRegister.setVisibility(View.INVISIBLE);
-                            }
-                        });
-                    }else{
-                        Toast.makeText(RegisterActivity.this, "Confirm password does not match", Toast.LENGTH_SHORT).show();
-                    }
-                }else{
-                    Toast.makeText(RegisterActivity.this, "Please fill in the fields", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
-        btnAlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendToLoginActivity();
-            }
-        });
     }
 
     @Override
@@ -101,6 +60,44 @@ public class RegisterActivity extends AppCompatActivity {
             sendToMainActivity();
         }
     }
+
+    @OnClick(R.id.btnCreateNewAccount)
+    public void createNewAccount() {
+        String txtRegisterEmailString = txtRegisterEmail.getText().toString();
+        String txtRegisterPasswordString = txtRegisterPassword.getText().toString();
+        String txtConfirmPasswordString = txtConfirmPassword.getText().toString();
+
+        if (!TextUtils.isEmpty(txtRegisterEmailString) && !TextUtils.isEmpty(txtRegisterPasswordString)
+                && !TextUtils.isEmpty(txtConfirmPasswordString)) {
+            if (txtRegisterPasswordString.equals(txtConfirmPasswordString)) {
+                probRegister.setVisibility(View.VISIBLE);
+                mAuth.createUserWithEmailAndPassword(txtRegisterEmailString, txtRegisterPasswordString)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    sendToMainActivity();
+                                } else {
+                                    String exceptionMsg = task.getException().getMessage();
+                                    Toast.makeText(RegisterActivity.this, "Error: " + exceptionMsg, Toast.LENGTH_LONG).show();
+                                }
+                                probRegister.setVisibility(View.INVISIBLE);
+                            }
+                        });
+            } else {
+                Toast.makeText(RegisterActivity.this, "Confirm password does not match", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(RegisterActivity.this, "Please fill in the fields", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick(R.id.btnAlreadyHaveAccount)
+    public void alreadyHaveAccount() {
+        sendToLoginActivity();
+    }
+
+
 
     private void sendToMainActivity() {
         Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
