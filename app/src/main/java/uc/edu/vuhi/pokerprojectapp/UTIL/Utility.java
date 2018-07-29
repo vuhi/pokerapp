20ac307f8354ab1ab68a3493f91764c80c277f93
vuhi <vuhi@mail.uc.edu>
2018-07-28 23:05:34 -0400

    - remove ResetPasswd activity - replace with dialog - add delay() to Utility - add Linear Layout for dialog - Fix constrain in Login activity

--- app/src/main/java/uc/edu/vuhi/pokerprojectapp/UTIL/Utility.java
+++ app/src/main/java/uc/edu/vuhi/pokerprojectapp/UTIL/Utility.java
@@ -2,7 +2,10 @@ package uc.edu.vuhi.pokerprojectapp.UTIL;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 
 public final class Utility {
 
@@ -11,5 +14,25 @@ public final class Utility {
         Intent intent = new Intent(currentActivity, otherActivityClass);
         currentActivity.startActivity(intent);
         currentActivity.finish();
+    }
+
+    public static void delay(final AlertDialog dialog, int time){
+        final Handler handler  = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (dialog.isShowing()) {
+                    dialog.dismiss();
+                }
+            }
+        };
+
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                handler.removeCallbacks(runnable);
+            }
+        });
+        handler.postDelayed(runnable, time);
     }
 }
