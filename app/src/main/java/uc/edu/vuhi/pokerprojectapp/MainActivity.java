d519ade8225f1002711dda57caa3028f43fa4a88
vuhi <vuhi@mail.uc.edu>
2018-07-31 03:35:24 -0400

    Add Card, Deck, Hand, handle deal(), shuffle, add cardImgs

--- app/src/main/java/uc/edu/vuhi/pokerprojectapp/MainActivity.java
+++ app/src/main/java/uc/edu/vuhi/pokerprojectapp/MainActivity.java
@@ -1,5 +1,7 @@
 package uc.edu.vuhi.pokerprojectapp;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -21,22 +23,35 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnItemSelected;
+import uc.edu.vuhi.pokerprojectapp.DTO.Card;
+import uc.edu.vuhi.pokerprojectapp.DTO.Deck;
+import uc.edu.vuhi.pokerprojectapp.DTO.Hand;
 import uc.edu.vuhi.pokerprojectapp.DTO.UserDTO;
 import uc.edu.vuhi.pokerprojectapp.UTIL.Utility;
 import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore mDatabase;
     private boolean isPlayed = false;
+    private List<ImageButton> imgButtons;
+    private Card card;
+    private Deck deck;
+    private Hand hand;
 
     @BindView(R.id.toolbarMain)
     Toolbar toolbarMain;
@@ -50,6 +65,21 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btnPlay)
     Button btnPlay;
 
+    @BindView(R.id.imgBtnCard1)
+    ImageButton imgBtnCard1;
+
+    @BindView(R.id.imgBtnCard2)
+    ImageButton imgBtnCard2;
+
+    @BindView(R.id.imgBtnCard3)
+    ImageButton imgBtnCard3;
+
+    @BindView(R.id.imgBtnCard4)
+    ImageButton imgBtnCard4;
+
+    @BindView(R.id.imgBtnCard5)
+    ImageButton imgBtnCard5;
+
     List<String> datas = new ArrayList<>();
 
     @Override
@@ -61,32 +91,97 @@ public class MainActivity extends AppCompatActivity {
         mDatabase = FirebaseFirestore.getInstance();
         setSupportActionBar(toolbarMain);
         getSupportActionBar().setTitle("Main Page");
+
+        card = new Card();
+        deck = new Deck(this);
+        hand = new Hand();
+        imgButtons = Arrays.asList(imgBtnCard1, imgBtnCard2, imgBtnCard3, imgBtnCard4, imgBtnCard5);
+
         loadDatas();
+        setBackCardImage();
+    }
+
+    private void setBackCardImage(){
+        //Set img
+        for (ImageButton imageButton : imgButtons) {
+            imageButton.setImageResource(getRandomBackCardImage());
+            imageButton.setEnabled(false);
+        }
+    }
+
+    private int getRandomBackCardImage(){
+        //Generate random number 0-5
+        Random r = new Random();
+        String index = String.valueOf(r.nextInt(5));
+        //Retrieve location of img by string name
+        int cardBackImgId = this.getResources().getIdentifier("back_0"+index, "drawable", this.getPackageName());
+        return cardBackImgId;
+    }
+
+    @OnClick({R.id.imgBtnCard1, R.id.imgBtnCard2, R.id.imgBtnCard3, R.id.imgBtnCard4,R.id.imgBtnCard5})
+    public void discardCard(View aView){
+        //Check the state of the card in hand
+        //This card is on hold
+        //->>>String id = aView.getTag()
+        if(!hand.cards[0].getIsDiscard()){
+            //discard it by showing the card back
+            hand.cards[0].setIsDiscard(true);
+            imgBtnCard1.setImageResource(getRandomBackCardImage());
+        }
+        //This card is already discard
+        else {
+            //hold the card by showing the card face
+            hand.cards[0].setIsDiscard(false);
+            imgBtnCard1.setImageResource(hand.cards[0].getCardImageId());
+        }
     }
 
     @OnClick(R.id.btnPlay)
-    public void Play(){
-        if(!isPlayed){
-            btnPlay.setText("DRAW");
+    public void play(){
+        //User play
+        isPlayed = true;
+        btnPlay.setText("DRAW");
+        //Shuffle card
+        deck.shuffle();
+        //Deal card 5 times to hand
+        for (int i = 0; i < imgButtons.size(); i++) {
+        card = deck.deal();
+            hand.PlaceCardInHand(card, i);
+            //Set img of card in hand
+            imgButtons.get(i).setImageResource(card.getCardImageId());
         }
-        //Deal random card
+        btnPlay.setEnabled(false);
+        scrollMoney.setSelectedItemPosition(0);
+        //Enable card to hold or discard
+        for (ImageButton imageButton : imgButtons) {
+            imageButton.setEnabled(true);
+        }
     }
 
     private void loadDatas() {
-        datas.add("000");
-        datas.add("111");
-        datas.add("222");
-        datas.add("333");
-        datas.add("444");
-        datas.add("555");
-        datas.add("666");
-        datas.add("777");
-        datas.add("888");
-        datas.add("999");
-        scrollMoney.addItems(datas,2);
+        datas.add("Select Bet");
+        datas.add("$10");
+        datas.add("$20");
+        datas.add("$30");
+        datas.add("$40");
+        datas.add("$50");
+        datas.add("$60");
+        datas.add("$70");
+        datas.add("$80");
+        datas.add("$90");
+        datas.add("$100");
+        scrollMoney.addItems(datas,0);
         scrollMoney.setOnItemSelectedListener(new ScrollChoice.OnItemSelectedListener() {
             @Override
             public void onItemSelected(ScrollChoice scrollChoice, int position, String name) {
+                if(isPlayed){
+                    if(position == 0){
+                        btnPlay.setEnabled(false);
+                    }
+                    else {
+                        btnPlay.setEnabled(true);
+                    }
+                }
                 txtViewNotification.setText(name);
             }
         });
